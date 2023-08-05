b2380c50619b7c73b3fa94108a527b3275e454c9
Pedro Silva <phsilva1@outlook.com.br>
2023-08-05 00:21:34 -0300

    Inserido o html

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return "Hello, Flask!llddddlddddll"
+def index():
+    return render_template('base.html')
 
 
-if __name__ == "__main___":
-    app.run(host='0.0.0.0', port=81, debug = True)
+if __name__ == "__main__":
+    app.run(debug=True)
